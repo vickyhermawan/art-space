b08b9fb354ae7adca9855f99752573e49da6a75c
vicky <vicky@ainosi.co.id>
2021-05-26 00:26:29 +0700

    init pwa and base project

---
+++ next.config.js
@@ -0,0 +1,34 @@
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+const prod = process.env.NODE_ENV === 'production'
+
+module.exports = {
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        use: {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next/static/sounds/",
+            outputPath: "static/sounds/",
+            name: "[name].[ext]",
+            esModule: false,
+          },
+        },
+      });
+  
+      return config;
+    },
+  };
+
+  module.exports = withPWA({
+    pwa: {
+      disable: prod ? false : true,
+      dest: 'public',
+      runtimeCaching,
+    },
+    devIndicators: {
+      autoPrerender: false,
+    },
+    future: { webpack5: true },
+  })
